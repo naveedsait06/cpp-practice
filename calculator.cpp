a9c66d83d57491bf7f212046f32f8b34435766f7
naveedsait06 <naveedsait06@gmail.com>
2025-12-20 00:13:54 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+    
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+': cout << num1 + num2; break;
+        case '-': cout << num1 - num2; break;
+        case '*': cout << num1 * num2; break;
+        case '/': 
+            if(num2 != 0) cout << num1 / num2;
+            else cout << "Division by zero error!";
+            break;
+        default: cout << "Invalid operator!";
+    }
+    return 0;
+}
