5ff0d1e49089a781b14577f1283b703cf2b14c67
naveedsait06 <naveedsait06@gmail.com>
2025-12-20 00:15:03 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str, rev = "";
+    cout << "Enter a string: ";
+    cin >> str;
+    for(int i = str.length() - 1; i >= 0; i--) rev += str[i];
+
+    if(str == rev) cout << str << " is a palindrome";
+    else cout << str << " is not a palindrome";
+    return 0;
+}
